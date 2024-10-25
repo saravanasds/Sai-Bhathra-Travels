37ff69a52ca0c38e32fc514a223156459818ea5a
karthikn1997 <karthikn951@gmail.com>
2024-10-25 17:15:09 +0530

    gg

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="-mr-2 flex md:hidden">
+                    {/* <div className="-mr-2 flex md:hidden">
                         <button
                             onClick={toggleNavbar}
                             className="inline-flex items-center justify-center p-2 rounded-md text-white focus:outline-none"
@@ -64,18 +64,18 @@ const Navbar = () => {
                                 </svg>
                             )}
                         </button>
-                    </div>
+                    </div> */}
                 </div>
             </div>
 
-            {isOpen && (
+            {/* {isOpen && (
                 <div className="md:hidden bg-gray-200 flex justify-center items-center">
                     <div className="px-2 py-2 pb-4 space-x-8 sm:px-3 text-[14px] sm:text-[16px] font-semibold">
                         <a href="#">Home</a>
                         <a href="#packages">Packages</a>
                     </div>
                 </div>
-            )}
+            )} */}
         </nav>
     );
 };
